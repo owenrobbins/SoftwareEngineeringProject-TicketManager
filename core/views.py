7b52676974cfc4cb35f199481f2bff8bc0baa9f3
Owen.Robbins <owen.robbins@paconsulting.com>
2026-02-02 22:05:11 +0000

    Creating base html template, and a home page structure

--- core/views.py
+++ core/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("TicketLite is running.")
-
+    return render(request, 'core/home.html')
