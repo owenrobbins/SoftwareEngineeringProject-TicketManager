0f17620624dd447207f741e23e06ed8d6336c8eb
Owen.Robbins <owen.robbins@paconsulting.com>
2026-02-16 22:29:00 +0000

    Adding in the basic framework for authentication, using Djangos built in templates as a basis for registering, logging in, and logging out.

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,17 @@
  # core/urls.py
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
-urlpatterns = [
-    # Maps the root URL to the home view
-    path('', views.home, name='home'),
-]
+app_name = 'core' # Best practice to separate namespaces
 
+urlpatterns = [
+    # Maps each URL pattern to corresponding view
+    # Custom Views
+    path('', views.home, name='home'), # Root URL (Home)
+    path('register/', views.register, name='register'), # Registration 
+    
+    # Built in Django Views
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'), # Login (specifying the specific auth view)
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'), # Logout
+]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,20 @@
 # core/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     # Renders in the hompage for TicketLite
     return render(request, 'core/home.html')
+
+def register(request):
+    # Handles user registration using Django built in User Creation Form.
+    if request.method == 'POST': # If the user is submitting the form
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('core:login') # Redirects the user to the login page after successfully registering user
+    
+    else: # If the user is just opening the Registration Form
+        form = UserCreationForm()
+    return render (request, 'core/register.html', {'form': form})
