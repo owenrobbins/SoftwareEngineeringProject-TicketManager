63eb61190e341efcddedfe916c8d99d3f57db242
Owen.Robbins <owen.robbins@paconsulting.com>
2026-02-22 22:14:29 +0000

    Setting up foundation for admin-specific controls. Also created the superuser, admin user and standard user within terminal / Django admin panel

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 # core/views.py
-from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
@@ -18,3 +19,12 @@ def register(request):
     else: # If the user is just opening the Registration Form
         form = UserCreationForm()
     return render (request, 'core/register.html', {'form': form})
+
+def is_admin(user):
+    # Function to check if a user is an admin, and return result
+    return user.is_staff
+
+@login_required
+@user_passes_test(is_admin)
+def delete_project(request, project_id): # Placeholder for an example admin request, with decorators to check login and admin privileges
+    return
