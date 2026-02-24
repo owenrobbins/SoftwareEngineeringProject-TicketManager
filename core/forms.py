cc0368e78dcd54a9db6fc1632e5da83ddb25842e
Owen.Robbins <owen.robbins@paconsulting.com>
2026-02-24 22:46:15 +0000

    Cleaning up the modal screen a bit

--- core/forms.py
+++ core/forms.py
@@ -6,3 +6,24 @@ class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ['title', 'description', 'project', 'status', 'priority', 'assigned_to']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+            }),
+            'project': forms.Select(attrs={
+                'class': 'form-select',
+            }),
+            'status': forms.Select(attrs={
+                'class': 'form-select',
+            }),
+            'priority': forms.Select(attrs={
+                'class': 'form-select',
+            }),
+            'assigned_to': forms.Select(attrs={
+                'class': 'form-select',
+            }),
+        }
